73ebde90f92a1b0a0c7f2c6a1caaef7883ada9bc
Saromerob <santiagoromero2105@gmail.com>
2025-02-04 19:23:05 -0500

    brands sections - sección de marcas

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import Services from "@/components/Services/services";
+import BrandsSection from "@/components/BrandSection/Brand";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main>
         <HeroSection />
         <Services />
+        <BrandsSection />
       </main>
     </div>
   );
---
+++ src/components/BrandSection/Brand.jsx
@@ -0,0 +1,88 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import styles from "./Brand.module.css"; // Importar el CSS module
+
+const brands = [
+  { name: "comap", src: "/images/logocomap.png", alt: "comap" },
+  { name: "abb", src: "/images/abb.jpg", alt: "abb" },
+  { name: "dosan", src: "/images/dosan.jpg", alt: "dosan" },
+  { name: "perkins", src: "/images/perkins.jpg", alt: "perkins" },
+  { name: "standford", src: "/images/standford.jpg", alt: "standford" },
+  { name: "tripplite", src: "/images/tripplite.jpg", alt: "tripplite" },
+];
+
+export default function BrandsSection() {
+  const sliderRef = useRef(null);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sliderRef.current) {
+        sliderRef.current.scrollLeft += isMobile ? 405 : 500; // Ajusta velocidad según el dispositivo
+        if (
+          sliderRef.current.scrollLeft >=
+          sliderRef.current.scrollWidth - sliderRef.current.clientWidth
+        ) {
+          sliderRef.current.scrollLeft = 0;
+        }
+      }
+    }, 2500);
+
+    return () => clearInterval(interval);
+  }, [isMobile]);
+
+  return (
+    <section className={styles.brandsSection}>
+      <h2 className={styles.brandsTitle}>Marcas Que Comercializamos</h2>
+      <div
+        className={styles.brandsSlider}
+        ref={sliderRef}
+        style={{
+          display: "flex",
+          overflow: "hidden",
+          justifyContent: "space-between",
+        }}
+      >
+        {isMobile
+          ? brands.map((brand, index) => (
+              <div
+                key={index}
+                className={styles.brandContainer}
+                style={{ minWidth: "100%", textAlign: "center" }}
+              >
+                <img
+                  src={brand.src}
+                  alt={brand.alt}
+                  className={styles.brandLogo}
+                  style={{ width: "150px", height: "auto" }}
+                />
+              </div>
+            ))
+          : brands.map((brand, index) => (
+              <div
+                key={index}
+                className={styles.brandContainer}
+                style={{ flex: "0 0 25%", textAlign: "center" }}
+              >
+                <img
+                  src={brand.src}
+                  alt={brand.alt}
+                  className={styles.brandLogo}
+                  style={{ width: "150px", height: "auto" }}
+                />
+              </div>
+            ))}
+      </div>
+    </section>
+  );
+}
